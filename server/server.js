39fa46cfe44b8eb112578e89370e5e28e7feddc0
devWeibel <weibel.jason@gmail.com>
2023-01-20 11:12:53 -0500

    session code in server.js for later

--- server/server.js
+++ server/server.js
@@ -2,15 +2,35 @@ const express = require('express');
 const path = require('path');
 const session = require('express-session');
 // const routes = require('./controllers');
+
 const sequelize = require('./config/connection');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// const sesh = {
+//     secret: 'Its for dogs',
+//     cookie: {
+//         maxAge: 300000,
+//         httpsOnly: true,
+//         secure: false, 
+//         sameSite: 'strict',
+//     },
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//         db: sequelize
+//     })
+// };
+
+// app.use(session(sesh));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
