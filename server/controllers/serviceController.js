43758686e8f0be7b4cddcce6d97fb0a833843378
devWeibel <weibel.jason@gmail.com>
2023-02-05 19:22:25 -0500

    initial REST API for services

--- server/controllers/serviceController.js
+++ server/controllers/serviceController.js
@@ -1,10 +1,13 @@
 const asyncHandler = require('express-async-handler');
 
+const Service = require('../models/serviceModel');
+
 // @desc    get services
 // @route   GET /api/service
 // @access Private
 const getService = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'get services' });
+    const services = await Service.find()
+    res.status(200).json({ services });
 });
 
 // @desc    set services
@@ -15,21 +18,46 @@ const setService = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('error message to be later determined')
     };
-    res.status(200).json({ message: 'set services' });
+
+    const service = await Service.create({
+        text: req.body.text,
+    })
+
+    res.status(200).json({ service });
 });
 
 // @desc    update services
 // @route   PUT /api/service/:id
 // @access Private
 const updateService = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `update services ${req.params.id}` })
+    const service = await Service.findById(req.params.id)
+
+    if(!service) {
+        res.status(400)
+        throw new Error('Service not found.')
+    };
+
+    const updatedService = await Service.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    });
+
+    res.status(200).json({ updatedService });
 });
 
 // @desc    delete services
 // @route   DELETE /api/service/:id
 // @access Private
 const deleteService = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `delete services ${req.params.id}` })
+    const service = await Service.findById(req.params.id)
+
+    if(!service) {
+        res.status(400)
+        throw new Error('Service not found.')
+    };
+
+    await service.remove();
+
+    res.status(200).json({ id: req.params.id })
 });
 
 module.exports = {
---
+++ server/models/serviceModel.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const serviceSchema = mongoose.Schema({
+    //work on this
+});
+
+module.exports = mongoose.model('Service', serviceSchema);
