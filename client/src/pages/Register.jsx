1af1e5a9371511838918f82c4f3b162857168d42
devWeibel <weibel.jason@gmail.com>
2023-02-21 09:48:52 -0500

    register form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -12,12 +12,30 @@ const Register = () => {
 
   const { name, email, password, password2 } = formData;
 
+  const onChange = () => {};
+
   return (
     <>
       <section className="heading">
         <h1>
-			<FaUser /> Register
-		</h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+      <section className="form">
+        <form>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              value={name}
+              placeholder="Your name here"
+              onChange={onChange}
+            />
+          </div>
+        </form>
       </section>
     </>
   );
