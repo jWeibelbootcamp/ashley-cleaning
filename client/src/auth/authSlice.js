51cdf4132578e1076ad98efbfe3a5a7386b693d2
devWeibel <weibel.jason@gmail.com>
2023-02-24 17:47:14 -0500

    more register.jsx work, spinner, redux reducers

--- client/src/auth/authSlice.js
+++ client/src/auth/authSlice.js
@@ -17,9 +17,10 @@ export const register = createAsyncThunk('auth/register', async (user, thunkAPI)
     try {
         return await authService.register(user)
     } catch (error) {
-        
-    }
-})
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message);
+    };
+});
 
 export const authSlice = createSlice({
     name: 'auth',
@@ -33,7 +34,23 @@ export const authSlice = createSlice({
             state.message = ''
         }
     },
-    extraReducers: () => {}
+    extraReducers: (builder) => {
+        builder
+            .addCase(register.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(register.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.user = action.payload
+            })
+            .addCase(register.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+                state.user = null
+            })
+    }
 });
 
 export const { reset } = authSlice.actions;
