82a9b8ee05e66c35e8e339c0026ff37a0bed3587
devWeibel <weibel.jason@gmail.com>
2023-03-24 15:31:05 -0400

    more login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,62 +1,99 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { FaSignInAlt } from "react-icons/fa";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { FaSignInAlt } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { FaUser } from 'react-icons/fa';
+import { login, reset } from '../auth/authSlice';
+import Spinner from '../components/Spinner';
 
 const Login = () => {
   const [formData, setFormData] = useState({
-    email: "",
-    password: "",
+    email: '',
+    password: '',
   });
 
   const { email, password } = formData;
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate('/');
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
   const onChange = (e) => {
-	// use previous state as argument and then spread across previous. target each event for key and value. 
-	setFormData( (prevState) => ({
-		...prevState, 
-		[e.target.name]: [e.target.value],
-	}))
+    // use previous state as argument and then spread across previous. target each event for key and value.
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: [e.target.value],
+    }));
   };
 
   const onSubmit = (e) => {
-	e.preventDefault()
+    e.preventDefault()
+
+    const userData = {
+      email,
+      password,
+    }
+
+    dispatch(login(userData));
+  };
+
+  if (isLoading) {
+    return <Spinner />;
   };
 
   return (
     <>
-      <section className="heading">
+      <section className='heading'>
         <h1>
           <FaSignInAlt /> Login
         </h1>
         <p>Login Below</p>
       </section>
-      <section className="form">
+      <section className='form'>
         <form onSubmit={onSubmit}>
-		  <div className="form-group">
+          <div className='form-group'>
             <input
-              type="email"
-              className="form-control"
-              id="email"
-              name="email"
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
               value={email}
-              placeholder="email"
+              placeholder='email'
               onChange={onChange}
             />
           </div>
-		  <div className="form-group">
+          <div className='form-group'>
             <input
-              type="password"
-              className="form-control"
-              id="password"
-              name="password"
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
               value={password}
-              placeholder="password"
+              placeholder='password'
               onChange={onChange}
             />
           </div>
-		  <div className="form-group">
-			<button type="submit" className="btn btn-block">Submit</button>
-		  </div>
+          <div className='form-group'>
+            <button type='submit' className='btn btn-block'>
+              Submit
+            </button>
+          </div>
         </form>
       </section>
     </>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -64,7 +64,7 @@ const Register = () => {
 
   if (isLoading) {
     return <Spinner />;
-  }
+  };
 
   return (
     <>
